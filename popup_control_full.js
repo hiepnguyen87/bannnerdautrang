cbecc7cda0b9721d1baa58b269701c7de8a7ecb3
hiepnguyen87 <hiepnguyen.ute@gmail.com>
2016-04-09 00:35:48 +0700

    Update popup_control_full.js

--- popup_control_full.js
+++ popup_control_full.js
@@ -1,5 +1,4 @@
-//please contact me giaiphapthuonghieu.org@gmail.com
-//website http://giaiphapthuonghieu.vn or http://faceseo.vn
+//please contact me trangtri360.com
 
 (function($) {
     $.fn.linhnguyen = function(options) {        
@@ -117,7 +116,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://trangtri360.com/lam-bang-quang-cao.html' target='_blank'><img src='http://trangtri360.com/images/promo/0/outdoor-sign-vietnam.png' width='500px'/>Thi công bảng hiệu quảng cáo chuyên nghiệp Thủ Đức.</a>. Bảo hành 24 tháng. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://trangtri360.com/lam-bang-quang-cao.html' target='_blank'><img src='http://trangtri360.com/images/promo/0/outdoor-sign-vietnam.png' width='500px'/>Thi công bảng quảng cáo chuyên nghiệp .</a>. B.hành 24 tháng. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
